15ecdee96bf981b32aa33f99eeb24e091a9e833c
Ryan <rhtakaha@calpoly.edu>
2022-11-07 15:33:49 -0800

    added the baseline api calls (subject to change)

--- expressjs-backend/api_calls.js
+++ expressjs-backend/api_calls.js
@@ -1,9 +1,10 @@
 const express = require('express');
+// const mongoose = require('mongoose');
 const cors = require('cors');
-const userServices = require('./models/dbCalls');
+const dbCalls = require('./models/dbCalls');
 
 const app = express();
-const port = 5000;
+// const port = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,10 +13,29 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+// create an item for a given user
+app.post('/user-items', async (req, res) => {
+  const item = req.body;
+  const { userId } = req.query;
+  if (userId === undefined) {
+    // if there was an error and the user isn't signed in
+    res.status(401).end();
+  } else {
+    // user signed in so add the new item
+    try {
+      await dbCalls.addItem(userId, item);
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).end();
+    }
+  }
 });
 
+// read the items of a given user
 app.get('/user-items', async (req, res) => {
   const { userId } = req.query;
   if (userId === undefined) {
@@ -23,7 +43,66 @@ app.get('/user-items', async (req, res) => {
     res.status(401).end();
   } else {
     // user signed in so send all their items
-    const result = await userServices.getUserItems();
-    res.send(result).status(200).end();
+    try {
+      const result = await dbCalls.getUserItems();
+      res.send(result).status(200).end();
+    } catch (error) {
+      res.status(404).end();
+    }
+  }
+});
+
+// read a particular item
+app.get('/user-items', async (req, res) => {
+  const { itemId } = req.query;
+  const { userId } = req.query;
+  if (userId === undefined) {
+    // if there was an error and the user isn't signed in
+    res.status(401).end();
+  } else {
+    // user signed in so get the item
+    try {
+      await dbCalls.getItem(userId, itemId);
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).end();
+    }
+  }
+});
+
+// update a particular item
+app.patch('/user-items', async (req, res) => {
+  const { itemId } = req.query;
+  const newItem = req.body;
+  const { userId } = req.query;
+  if (userId === undefined) {
+    // if there was an error and the user isn't signed in
+    res.status(401).end();
+  } else {
+    // user signed in so modify the item
+    try {
+      await dbCalls.updateItem(userId, itemId, newItem);
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).end();
+    }
+  }
+});
+
+// delete a particular item
+app.patch('/user-items', async (req, res) => {
+  const { itemId } = req.query;
+  const { userId } = req.query;
+  if (userId === undefined) {
+    // if there was an error and the user isn't signed in
+    res.status(401).end();
+  } else {
+    // user signed in so modify the item
+    try {
+      await dbCalls.deleteItem(userId, itemId);
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).end();
+    }
   }
 });
