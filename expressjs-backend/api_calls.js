88f152690a7da7ed362bccc2850c14fc4d2e51e0
Ryan <rhtakaha@calpoly.edu>
2022-11-07 15:12:44 -0800

    some more api changes

--- expressjs-backend/api_calls.js
+++ expressjs-backend/api_calls.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const cors = require('cors');
 const userServices = require('./user_services');
 
 const app = express();
-const port = 5000;
+// const port = 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -13,10 +13,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
 
+// read
 app.get('/user-items', async (req, res) => {
   const { userId } = req.query;
   if (userId === undefined) {
@@ -24,7 +25,29 @@ app.get('/user-items', async (req, res) => {
     res.status(401).end();
   } else {
     // user signed in so send all their items
-    const result = await userServices.getUserItems();
-    res.send(result).status(200).end();
+    try {
+      const result = await userServices.getUserItems();
+      res.send(result).status(200).end();
+    } catch (error) {
+      res.status(404).end();
+    }
+  }
+});
+
+// create
+app.post('/user-items', async (req, res) => {
+  const item = req.body;
+  const { userId } = req.query;
+  if (userId === undefined) {
+    // if there was an error and the user isn't signed in
+    res.status(401).end();
+  } else {
+    // user signed in so add the new item
+    try {
+      await userServices.addItem(userId, item);
+      res.status(204).end();
+    } catch (error) {
+      res.status(400).end();
+    }
   }
 });
